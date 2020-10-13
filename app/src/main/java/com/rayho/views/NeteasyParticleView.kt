0c34f5747c35163510600ea7498f0d5d9b7a360d
huangweihao <rayho360@gmail.com>
2020-10-13 14:39:41 +0800

    添加注释

--- app/src/main/java/com/rayho/views/NeteasyParticleView.kt
+++ app/src/main/java/com/rayho/views/NeteasyParticleView.kt
@@ -18,6 +18,7 @@ import kotlin.system.measureTimeMillis
  * @Description:
  * @Author:         huangweihao
  * @CreateDate:     2020/9/27
+ * 具体实现参考https://juejin.im/post/6871049441546567688#comment
  */
 class NeteasyParticleView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     private var mHeight = 0f
@@ -44,6 +45,7 @@ class NeteasyParticleView(context: Context, attrs: AttributeSet) : View(context,
         animator.duration = 2000
         animator.interpolator = LinearInterpolator()
         animator.addUpdateListener {
+            //动画过程中 更新粒子的状态
             updateParticle()
             invalidate()
         }
@@ -54,7 +56,9 @@ class NeteasyParticleView(context: Context, attrs: AttributeSet) : View(context,
         super.onDraw(canvas)
         val time = measureTimeMillis {
             particleList.forEachIndexed { index, particle ->
+                //改变粒子的透明度
                 paint.alpha = ((1f - particle.offset / particle.maxOffset) * 225f).toInt()
+                //画粒子
                 canvas?.drawCircle(particle.x, particle.y, particleRadius, paint)
             }
         }
@@ -65,35 +69,51 @@ class NeteasyParticleView(context: Context, attrs: AttributeSet) : View(context,
     private fun updateParticle() {
         particleList.forEach {
             if (it.offset > it.maxOffset) {
+                //粒子的偏移量大于最大移动距离时候 重置粒子的状态
                 it.offset = 0f
                 it.speed = Random.nextInt(6).toFloat() + 1.5f
                 it.maxOffset = Random.nextInt(250).toFloat()
             }
+            //扩散圆上粒子的坐标(粒子从内圆移动到扩散圆 内圆圆心的反方向移动)
+            //具体坐标的计算参考链接文章
             it.x = (mWidth / 2 + cos(it.angle) * (it.offset + innerRadius)).toFloat()
             it.y = if (it.y > mHeight / 2) {
                 (sin(it.angle) * (innerRadius + it.offset) + mHeight / 2).toFloat()
             } else {
                 (mHeight / 2 - sin(it.angle) * (innerRadius + it.offset)).toFloat()
             }
+            // (速度 = 距离 / 时间 因为在for循环中改变每个粒子的状态
+            // 因此时间相当于不存在 因此粒子的移动距离等于offset += speed)
             it.offset += it.speed
         }
     }
 
 
+    //在改方法中 初始化粒子的状态
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
         mHeight = h.toFloat()
         mWidth = w.toFloat()
+        //添加圆的路径 但是不画圆
         path.addCircle(mWidth / 2, mHeight / 2, innerRadius, Path.Direction.CCW)
         pathMeasure.setPath(path, false)
+        //初始化每个粒子的状态
         for (i in 0..particleNumber) {
+            //按比例测量内圆路径上每一点的值
             pathMeasure.getPosTan(i / particleNumber.toFloat() * pathMeasure.length, pos, tan)
+            //X值随机偏移
             val x = Random.nextInt(6) -3f + pos[0]
+            //Y值随机偏移
             val y = Random.nextInt(6) - 3f + pos[1]
+            //通过反余弦函数获取粒子的角度
             val angle = acos(((pos[0] - mWidth / 2) / innerRadius)).toDouble()
+            //随机粒子的偏移量
             val offset = Random.nextInt(200)
+            //随机粒子的速度
             val speed = Random.nextInt(2) + 0.5f
+            //随机粒子的最大移动距离
             val maxOffset = Random.nextInt(250)
+            //添加粒子
             particleList.add(
                 Particle(
                     x,
--- app/src/main/java/com/rayho/views/Particle.kt
+++ app/src/main/java/com/rayho/views/Particle.kt
@@ -6,11 +6,11 @@ package com.rayho.views
  * @CreateDate:     2020/9/29
  */
 data class Particle(
-    var x: Float,
-    var y: Float,
-    var radius: Float,
-    var offset: Float,
-    var speed: Float,
-    var maxOffset: Float,
-    var angle: Double
+    var x: Float, //粒子的x坐标
+    var y: Float, //粒子的y坐标
+    var radius: Float, //粒子的半径
+    var offset: Float, //粒子的偏移量
+    var speed: Float, //粒子的速度
+    var maxOffset: Float, //粒子的最大移动距离
+    var angle: Double //粒子的角度
 )
